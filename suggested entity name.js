3287c926eb4cf2923e0266f7affe099dab4e957b
kamille <168681438+KamilleMendoza@users.noreply.github.com>
2025-05-29 14:09:54 -0600

    Update suggested entity name.js
    updated March 2025

--- suggested entity name.js
+++ suggested entity name.js
@@ -1,25 +1,60 @@
-var grid = FeatureSetByPortalItem(Portal('***'), '***', 0, ['objectid','uuid'])
+//well sticks
+
+//Updated Suggested Entity Name Calculator
+var grid = FeatureSetByPortalItem(Portal('***'), 'itemID', 0, ['objectid','field'])
 var last_point = Geometry($feature).paths[-1][-1]
 var lsd_now = First(Intersects(grid,last_point))
 
 
-
+//bh layer - mosaic
 var stickbh = FeatureSetByName($map, "Wells BH",['*'])
-//"MOSAIC STICK BH COUNT P F"
+
 //var stickbhCount = Count(Intersects(lsd_now, $feature))
 var stickbhCount = Count(Intersects(lsd_now, stickbh))
 //return stickbhCount
 
-var bhl = FeatureSetByPortalItem(Portal('***'),'***', 0,  ['CPA_UWI'])
+//well bh - WELLS_BH_P - Return all BH points
+//var bhl = FeatureSetByPortalItem(Portal('***'),'itemID', 0,  ['CPA_UWI'], "RIGHT('uwi',1)=0")
+var bhl = FeatureSetByName($map,'Bottom Hole Locations', ['CPA_UWI'], true);
+
+//Count all bh wells inside the LSD
 var bhlCount = Count(Intersects(lsd_now, bhl))
 
+//Count all vert and lateral wells inside the LSD
 var totalCt = stickbhCount + bhlCount
+//var skWells = Count(Intersects(lsd_now, stickbh)) + 1
+
 var next_event = stickbhCount + bhlCount + 1
 
-var output = Text(totalCt, '00')
-     if (totalCt == 0) {return Text("100/" + lsd_now.uuid + "/00")}
-     if (totalCt == 1) {return Text("102/" + lsd_now.uuid + "/00")}
-     if (totalCt >= 10) {return "1"+totalCt + "/" +  lsd_now.uuid + "/00"}
-     else {return Text("10"+ next_event +"/" + lsd_now.uuid + "/00")}
+//province - SDE_RO data tsore
+var province= First(Intersects($feature,FeatureSetByPortalItem(Portal('***'), 'itemID', 0, ['prov_name'], true)))
+var prov1 = province.prov_name
 
+
+//var output = Text(next_event+ "/" + lsd_now.uuid + "/00")
+var output = ""
+
+if (totalCt == 0) {
+  if (prov1 == 'Alberta'){return Text("00/" + lsd_now.uuid + "/0")}
+  if (prov1 == 'Saskatchewan') {return Text("0" + next_event+ "/" + lsd_now.uuid + "/0")}
+}
+
+if (totalCt == 1) {
+  if (prov1 == 'Alberta'){return Text("02/"+ lsd_now.uuid + "/0")}
+  if (prov1 == 'Saskatchewan') {return Text("0" + next_event+ "/" + lsd_now.uuid + "/0")}
+}
+
+if (totalCt > 1 || totalCt < 10){
+  if (prov1 == 'Alberta') {return Text("0" + next_event+ "/" + lsd_now.uuid + "/0")}
+  if (prov1 == 'Saskatchewan') {return Text("0" + next_event+ "/" + lsd_now.uuid + "/0")}
+}
+
+if (totalCt > 10){
+  if (prov1 == 'Alberta') {return Text("0"+ next_event+ "/" + lsd_now.uuid + "/0")}
+  if (prov1 == 'Saskatchewan'){return Text(next_event+ "/" + lsd_now.uuid + "/0")}
+  }
+
+else return Text(next_event+ "/" + lsd_now.uuid + "/0")
 return output
+
+
